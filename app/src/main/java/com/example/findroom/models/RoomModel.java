a6f2d6d45879fa13ebe7f8b21d079634e40e53e5
thehieuduong7 <hieuduon959@gmail.com>
2022-05-13 12:12:08 +0700

    update model firebase

---
+++ app/src/main/java/com/example/findroom/models/RoomModel.java
@@ -0,0 +1,108 @@
+package com.example.findroom.models;
+
+import android.media.Image;
+
+import java.util.List;
+
+public class RoomModel {
+    private int id;
+    private String name;
+    private String type;
+    private float price;
+    private String status;
+    private float area;
+    private float deposit; // tien coc
+    private String location;
+    private String note;
+    private List<Image> images;
+
+    public RoomModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public float getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(float deposit) {
+        this.deposit = deposit;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public List<Image> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Image> images) {
+        this.images = images;
+    }
+    @Override
+    public String toString() {
+        return "RoomModel{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", status='" + status + '\'' +
+                ", area=" + area +
+                ", deposit=" + deposit +
+                ", location='" + location + '\'' +
+                ", note='" + note + '\'' +
+                ", images=" + images +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/findroom/models/UserModel.java
@@ -0,0 +1,58 @@
+package com.example.findroom.models;
+
+import java.util.List;
+
+public class UserModel {
+    private String username;
+    private String password;
+    private String fullName;
+    private List<Integer> myRoomId;
+    private List<Integer> savedRoomId;
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public List<Integer> getMyRoomId() {
+        return myRoomId;
+    }
+
+    public void setMyRoomId(List<Integer> myRoomId) {
+        this.myRoomId = myRoomId;
+    }
+
+    public List<Integer> getSavedRoomId() {
+        return savedRoomId;
+    }
+
+    public void setSavedRoomId(List<Integer> savedRoomId) {
+        this.savedRoomId = savedRoomId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "username='" + username + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
