a631ed71f8028ab377b84f09586607181c2f21a2
thehieuduong7 <hieuduon959@gmail.com>
2022-05-14 09:15:20 +0700

    update model firebase

---
+++ app/src/main/java/com/example/findroom/models/view/login/RegisterView.java
@@ -0,0 +1,14 @@
+package com.example.findroom.models.view.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_view);
+    }
+}
