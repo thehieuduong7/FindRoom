0b0c18cba0eff84617e3280a9a16c5910271ef9c
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-05-25 16:30:06 +0700

    update itemroom

--- app/src/main/java/com/example/findroom/models/view/login/RegisterView.java
+++ app/src/main/java/com/example/findroom/models/view/login/RegisterView.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.findroom.R;
+
 public class RegisterView extends AppCompatActivity {
 
     @Override
