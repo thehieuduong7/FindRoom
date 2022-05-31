d6c01456387c14bce085a534b6ff49c573c51405
phuc-noob <57171615+phuc-noob@users.noreply.github.com>
2022-05-31 22:26:47 +0700

    fix getResources() function

--- app/src/main/java/com/example/findroom/controler/ImageConvert.java
+++ app/src/main/java/com/example/findroom/controler/ImageConvert.java
@@ -1,16 +1,24 @@
 package com.example.findroom.controler;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.findroom.R;
+
 
 import java.io.ByteArrayOutputStream;
 
 public class ImageConvert extends AppCompatActivity {
+    private Context context ;
+
+    public ImageConvert(Context context) {
+        this.context = context;
+    }
+
     public Bitmap ConvertStringToBitmapImage(String imgString){
         byte[] decodedString = Base64.decode(imgString, Base64.DEFAULT);
         Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
@@ -19,7 +27,7 @@ public class ImageConvert extends AppCompatActivity {
 
     public String convertImageToString(){
         Bitmap bitmapImage;
-        bitmapImage = BitmapFactory.decodeResource(getResources(), R.drawable.download);
+        bitmapImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.download);
         String imgString = Base64.encodeToString(getBytesFromBitmap(bitmapImage),
                 Base64.NO_WRAP);
         return imgString;
